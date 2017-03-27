4a318009c5f288ba2d298e45600891fe4c44cdc3
wuxinxi <wu_tangren@163.com>
2017-03-27 17:50:35 +0800

    Signed-off-by: wuxinxi <wu_tangren@163.com>

---
+++ app/src/main/java/com/wxx/greendao/utils/Test.java
@@ -0,0 +1,14 @@
+package com.wxx.greendao.utils;
+
+/**
+ * 作者：Tangren_ on 2017/3/27 0027.
+ * 邮箱：wu_tangren@163.com
+ * TODO:用一句话概括
+ */
+
+
+public class Test {
+    public void get() {
+    }
+
+}
